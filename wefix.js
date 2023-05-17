b8deaca7513038f68860946bd0dee230ed75a4ac
Musty$dev <90248827+mustaphaolaonipekun@users.noreply.github.com>
2023-05-17 18:27:59 +0100

    Add files via upload

---
+++ wefix.js
@@ -0,0 +1,31 @@
+// const slides = document.querySelectorAll('slide');
+// const right = document.getElementById('right');
+// const left = document.getElementById('left');
+// const carousel = document.getElementById('carousel');
+
+// const SLIDES_COUNT = slides.length;
+
+// left.addEventListener('click', () => {
+//     current_slide++;
+//     if (current_slide > SLIDES_COUNT - 1) {
+//         current_slide = 0;
+//     }
+//     updateCarousel();
+// });
+// right.addEventListener('click', () => {
+//     current_slide--;
+//     if (current_slide > SLIDES_COUNT) {
+//         current_slide = 0;
+//     }
+//     updateCarousel();
+// });
+
+// function updateCarousel() {
+//     carousel.style.transform = `translate X(${-current_slide * slides[0].offsetwidth}px)`
+// }
+async src='https://d2mpatx37cqexb.cloudfront.net/delightchat-whatsapp-widget/embeds/embed.min.js'>
+
+var wa_btnSetting = {"btnColor":"cyan","ctaText":"WhatsApp Live Chat","cornerRadius":6,"marginBottom":2,"marginLeft":0,"btnPosition":"left","whatsAppNumber":"2348117863988","welcomeMessage":"Hi am intrested in *Wefix Automation*. How can we proceed?","btnColorScheme":"light"};
+window.onload = () => {
+_waEmbed(wa_btnSetting);
+};
